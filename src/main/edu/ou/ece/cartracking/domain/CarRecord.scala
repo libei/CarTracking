0015e7ad0cce744319f07292a04ac993f1a30fd3
Bei <libei.twer@gmail.com>
2010-04-14 16:00:14 -0500

    run scripts in sql directory upon test phase

--- src/main/edu/ou/ece/cartracking/domain/CarRecord.scala
+++ src/main/edu/ou/ece/cartracking/domain/CarRecord.scala
@@ -1,6 +1,12 @@
 package edu.ou.ece.cartracking
 
+import javax.persistence._
+
+
+@Entity
 class CarRecord() {
+  @Id
+  @GeneratedValue
   var id: Long = 0l
 
 }
--- src/main/sql/create.sql
+++ src/main/sql/create.sql
@@ -1,9 +1,11 @@
 
-DROP DATABASE IF EXISTS CarTracking
-CREATE DATABASE CarTracking
+DROP DATABASE IF EXISTS CarTracking;
+CREATE DATABASE CarTracking;
+
+USE CarTracking;
 
 CREATE TABLE CarRecord
 (
-
+    id INT(11) NOT NULL auto_increment,
+    PRIMARY KEY (id)
 )
-
