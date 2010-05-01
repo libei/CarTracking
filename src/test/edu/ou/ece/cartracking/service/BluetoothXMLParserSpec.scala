129c7a9a221af31831e6caf6e1c26a270c224868
Bei <libei.twer@gmail.com>
2010-05-01 12:58:01 -0500

    finished the parser

--- src/test/edu/ou/ece/cartracking/service/BluetoothXMLParserSpec.scala
+++ src/test/edu/ou/ece/cartracking/service/BluetoothXMLParserSpec.scala
@@ -4,6 +4,7 @@ import org.scalatest.matchers.ShouldMatchers
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FlatSpec
+import edu.ou.ece.cartracking.domain.BluetoothRecord
 
 
 //<records>
@@ -29,24 +30,37 @@ class BluetoothXMLParserSpec extends FlatSpec with ShouldMatchers {
 
   it should "parse xml and get records" in {
 
-    val records = BluetoothXMLParser.parse(""""
-<records>
-  <record>
-    <unixTime>123</unixTime>
-    <macAddress>abc</macAddress>
-    <deviceType>cde</deviceType>
-    <unitSN>edf</unitSN>
-  </record>
-  <record>
-    <unixTime>098</unixTime>
-    <macAddress>fgh</macAddress>
-    <deviceType>hij</deviceType>
-    <unitSN>jkl</unitSN>
-  </record>
-</records>""")
+    val records = BluetoothXMLParser.parse(
+      """<?xml version="1.0"?>
+      <records>
+        <record>
+          <unixTime>123</unixTime>
+          <macAddress>abc</macAddress>
+          <deviceType>cde</deviceType>
+          <unitSN>edf</unitSN>
+        </record>
+        <record>
+          <unixTime>098</unixTime>
+          <macAddress>fgh</macAddress>
+          <deviceType>hij</deviceType>
+          <unitSN>jkl</unitSN>
+        </record>
+      </records>""")
 
     records.length should equal(2)
 
+    shouldContain(records, "hij", "fgh", "jkl", 98)
+    shouldContain(records, "cde", "abc", "edf", 123)
+  }
+
+  def shouldContain(records: List[BluetoothRecord], deviceType: String, macAddress: String, unitSN: String, unixTime: Integer) {
+    records.foreach(r => {
+
+      if ((r.deviceType == deviceType) && (r.macAddress == macAddress) && (r.unitSN == unitSN) && (r.unixTime == unixTime))
+        return
+
+    })
+    assert(false)
   }
 
 
