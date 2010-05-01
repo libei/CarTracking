4056023b1924645a77ee7165ac8d1f6f726596cb
Bei Li <bei@bei-laptop.(none)>
2010-05-01 12:05:42 -0500

    parse xml returned by the blusensor server

--- src/main/edu/ou/ece/cartracking/service/BluetoothXMLParser.scala
+++ src/main/edu/ou/ece/cartracking/service/BluetoothXMLParser.scala
@@ -2,17 +2,27 @@ package edu.ou.ece.cartracking.service
 
 import edu.ou.ece.cartracking.domain.BluetoothRecord
 import xml.XML
+import collection.mutable.ListBuffer
 
 object BluetoothXMLParser {
   def parse(rawXml: String): List[BluetoothRecord] = {
     if (rawXml.length == 0)
       return List();
 
-    var xml = XML.load rawXml;
+    var xml = XML loadString rawXml;
 
-    //    for (record <- (xml \\ "record")) {
-    //      record \ "unixTime").text
-    //    }
+    var records = new ListBuffer[BluetoothRecord]
+
+    for (record <- (xml \\ "record")) {
+      var r = new BluetoothRecord
+      r.unixTime = (record \ "unixTime").text.toInt
+      r.macAddress = (record \ "macAddress").text
+      r.deviceType = (record \ "deviceType").text
+      r.unitSN = (record \ "unitSN").text
+      records += r
+    }
+
+    records.toList
 
   }
 
