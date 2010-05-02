4c3e07df593d531d7e1847fdf5ec351b56b3f8b2
Bei Li <bei@bei-laptop.(none)>
2010-05-02 12:31:01 -0500

    send request to bluetooth sensor and parse the result

--- src/main/edu/ou/ece/cartracking/service/BluetoothSensor.scala
+++ src/main/edu/ou/ece/cartracking/service/BluetoothSensor.scala
@@ -1,6 +1,9 @@
 package edu.ou.ece.cartracking.service
 
 import edu.ou.ece.cartracking.domain.BluetoothRecord
+import java.util.Date
+import edu.ou.ece.cartracking.utils.HttpClientUtils
+import java.lang.String
 
 class BluetoothSensor(uri: String) {
 
@@ -8,8 +11,14 @@ class BluetoothSensor(uri: String) {
   //
   // paras:  ?start=xx&end=xx
 
-  def getRecords(): List[BluetoothRecord] = {
-    null
+  def getRecords(start: Date, end: Date): List[BluetoothRecord] = {
+
+    val unixStart = start.getTime / 1000L
+    val unixEnd = end.getTime / 1000L
+
+    val requestUrl = uri + "?start=" + unixStart + "&end=" + unixEnd
+    val xml = HttpClientUtils.get(requestUrl)
+    return BluetoothXMLParser.parse(xml)
   }
 
 }
---
+++ src/test/edu/ou/ece/cartracking/service/BluetoothSensorSpec.scala
@@ -0,0 +1,13 @@
+package edu.ou.ece.cartracking.service
+
+import org.junit.runner.RunWith
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class BluetoothSensorSpec extends FlatSpec with ShouldMatchers {
+
+  //  unixTime = System.currentTimeMillis() / 1000L;
+
+}
