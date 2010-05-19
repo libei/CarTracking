fd204f62b80e41041915582dc5c6989800b589bd
Bei Li <bei@bei-laptop.(none)>
2010-05-18 20:54:51 -0500

    * parse license plate file name to extract date / confidence and etc

--- src/main/edu/ou/ece/cartracking/service/LicensePlateParser.scala
+++ src/main/edu/ou/ece/cartracking/service/LicensePlateParser.scala
@@ -1,14 +1,25 @@
 package edu.ou.ece.cartracking.service
 
 import edu.ou.ece.cartracking.domain.LicensePlateRecord
+import java.text.SimpleDateFormat
 
 object LicensePlateParser {
   def parse(string: String): LicensePlateRecord = {
+    val firstComma: Int = string.indexOf(",")
+    val secondComma: Int = string.indexOf(",", firstComma + 1)
+    val thirdComma: Int = string.indexOf(",", secondComma + 1)
+    val fourthComma: Int = string.indexOf(",", thirdComma + 1)
+    val fifthComma: Int = string.indexOf(",", fourthComma + 1)
 
     val record = new LicensePlateRecord
-    record.number = "13JL"
-    record.accuracy = "71"
+    record.number = string.substring(thirdComma + 1, fourthComma)
+    record.accuracy = string.substring(fourthComma + 1, fifthComma)
+
+    var df = new SimpleDateFormat("MMdd,HHmmSSS");
+    record.date = df.parse(string.substring(1, secondComma))
+
     record
+
   }
 
 }
--- src/test/edu/ou/ece/cartracking/service/LicensePlateParserSpec.scala
+++ src/test/edu/ou/ece/cartracking/service/LicensePlateParserSpec.scala
@@ -4,6 +4,7 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FlatSpec
 import org.junit.runner.RunWith
+import java.util.{Calendar, GregorianCalendar}
 
 @RunWith(classOf[JUnitRunner])
 class LicensePlateParserSpec extends FlatSpec with ShouldMatchers {
@@ -11,6 +12,20 @@ class LicensePlateParserSpec extends FlatSpec with ShouldMatchers {
     val record = LicensePlateParser.parse("f0514,2138316,1,13JL,71,242,0,NONE,0,0,8,1,180,36.jpg")
     record.number should equal("13JL")
     record.accuracy should equal("71")
+
+    val calendar = new GregorianCalendar
+    calendar.setTime(record.date)
+    val month: Int = calendar.get(Calendar.MONTH)
+    val day: Int = calendar.get(Calendar.DAY_OF_MONTH)
+    val hour: Int = calendar.get(Calendar.HOUR_OF_DAY)
+    val minute: Int = calendar.get(Calendar.MINUTE)
+    val millisecond: Int = calendar.get(Calendar.MILLISECOND)
+
+    month should equal(4) //4 is actually 5th month
+    day should equal(14)
+    hour should equal(21)
+    minute should equal(38)
+    millisecond should equal(316)
   }
 
 }
