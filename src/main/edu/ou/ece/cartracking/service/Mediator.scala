c89557a2ab614a68b6c06a6b43c7175fceb65528
Bei <libei.twer@gmail.com>
2010-05-19 16:53:11 -0500

    * integrate license plate

--- src/main/edu/ou/ece/cartracking/service/Mediator.scala
+++ src/main/edu/ou/ece/cartracking/service/Mediator.scala
@@ -3,7 +3,7 @@ package edu.ou.ece.cartracking.service
 import actors.Actor
 import org.apache.log4j.Logger
 import edu.ou.ece.cartracking.repository.CarRecordRepository
-import edu.ou.ece.cartracking.domain.{CarRecord}
+import edu.ou.ece.cartracking.domain.{LicensePlateRecord, CarRecord}
 
 class Mediator(bluetoothSensor: BluetoothSensor, licensePlateDetector: LicensePlateDetector) extends Actor {
   private val logger: Logger = Logger.getLogger(getClass)
@@ -16,11 +16,17 @@ class Mediator(bluetoothSensor: BluetoothSensor, licensePlateDetector: LicensePl
         case c: CarIdentifiedMsg => {
           logger debug ("CarIdentifiedMsg received.")
 
-          val carRecord: CarRecord = new CarRecord
-          val records = bluetoothSensor.getRecords(null, null)
-          carRecord.addBluetoothRecords(records)
-          CarRecordRepository.save(carRecord)
+          //c.time
 
+          val carRecord = new CarRecord
+
+          val bluetoothRecords = bluetoothSensor.getRecords(null, null)
+          carRecord.addBluetoothRecords(bluetoothRecords)
+
+          val licensePlateRecords = licensePlateDetector.get(null, null)
+          carRecord.addLicensePlateRecords(licensePlateRecords)
+
+          CarRecordRepository.save(carRecord)
         }
 
       }
